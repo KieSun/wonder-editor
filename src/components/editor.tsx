7bc0dac144f51541423bb49a6cb60b0d1aa3a7c5
xuwu <zx597813039@gmail.com>
2021-05-29 22:03:39 +0800

    feat: 格式化 markdown 功能

--- src/components/editor.tsx
+++ src/components/editor.tsx
@@ -1,8 +1,11 @@
-import { useState, useCallback, useEffect } from 'react';
+import { useState, useCallback, useEffect, useRef } from 'react';
 import { Editor } from '@bytemd/react';
 import footnotes from '@bytemd/plugin-footnotes';
 // @ts-ignore
 import zhHans from 'bytemd/lib/locales/zh_Hans.json';
+import CodeMirror from 'codemirror';
+import prettier from 'prettier';
+import markdownPlugin from 'prettier/parser-markdown';
 import { fileToBase64, uploadFile } from '@/utils/upload';
 import keydown from '@/common/keyCode';
 
@@ -13,21 +16,26 @@ interface IEditorProps {
 export default (props: IEditorProps) => {
   const { handleEditorChange } = props;
   const [value, setValue] = useState('');
+  const [editor, setEditor] = useState<CodeMirror.Editor>();
 
-  useEffect(() => {
-    document.addEventListener('keydown', (event) => {
-      const { keyCode, ctrlKey, metaKey } = event;
-      if (keyCode === keydown.S && (ctrlKey || metaKey)) {
-        event.preventDefault();
-      }
-    });
-  }, []);
-
-  const handleChange = useCallback((instance) => {
-    const value = instance.getValue();
+  const handleChange = useCallback((value: string) => {
     setValue(value);
     handleEditorChange(value);
   }, []);
+
+  const handleFormatter = useCallback(() => {
+    const formattedValue = prettier.format(value, {
+      parser: 'markdown',
+      plugins: [markdownPlugin],
+    });
+    handleChange(formattedValue);
+    setTimeout(() => {
+      if (editor) {
+        const line = editor.lastLine() - 1;
+        editor?.setCursor(line, editor.getLineTokens(line).length);
+      }
+    });
+  }, [value, handleChange, editor]);
 
   const handleUploadImages = useCallback((files: File[]) => {
     return Promise.all(
@@ -44,6 +52,25 @@ export default (props: IEditorProps) => {
     );
   }, []);
 
+  useEffect(() => {
+    const listener = (event: KeyboardEvent) => {
+      const { keyCode, ctrlKey, metaKey } = event;
+      if (keyCode === keydown.S && (ctrlKey || metaKey)) {
+        event.preventDefault();
+        handleFormatter();
+      }
+    };
+    document.addEventListener('keydown', listener);
+    return () => {
+      document.removeEventListener('keydown', listener);
+    };
+  }, [handleFormatter]);
+
+  useEffect(() => {
+    const instance = (document.querySelector('.CodeMirror') as any)?.CodeMirror;
+    setEditor(instance);
+  }, []);
+
   return (
     <Editor
       mode="split"
@@ -51,9 +78,7 @@ export default (props: IEditorProps) => {
       locale={zhHans}
       plugins={[footnotes()]}
       uploadImages={handleUploadImages}
-      onChange={(v) => {
-        setValue(v);
-      }}
+      onChange={handleChange}
     />
   );
 };
